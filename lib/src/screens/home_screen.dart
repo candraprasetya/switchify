82858e980d63910aebe6f893482a114710910ba1
Candra Ramesthy <candraramadhanp@gmail.com>
2022-11-08 20:33:24 +0700

    revamp home screen

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -6,32 +6,70 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocConsumer<UserBloc, UserState>(
-        listener: (context, state) {
-          if (state is UserIsFailed) {
-            Commons().showSnackBar(context, state.message);
-          } else if (state is UserIsLogOut) {
-            context.go(routeName.login);
-          }
-        },
-        builder: (context, state) {
-          if (state is UserIsLoading) {
-            return const CircularProgressIndicator().centered();
-          } else if (state is UserIsSuccess) {
-            return VStack([
-              'Selamat Datang ${state.data.username}'.text.makeCentered(),
-              16.heightBox,
-              ButtonWidget(
-                onPressed: () {
-                  BlocProvider.of<UserBloc>(context).add(LogOutUser());
-                },
-                text: 'Log Out',
-              )
-            ]);
-          }
-          return 0.heightBox;
-        },
-      ).p16().centered(),
+      body: SafeArea(
+        child: BlocConsumer<UserBloc, UserState>(
+          listener: (context, state) {
+            if (state is UserIsFailed) {
+              Commons().showSnackBar(context, state.message);
+            } else if (state is UserIsLogOut) {
+              context.go(routeName.login);
+            }
+          },
+          builder: (context, state) {
+            if (state is UserIsLoading) {
+              return const CircularProgressIndicator().centered();
+            } else if (state is UserIsSuccess) {
+              return VStack(
+                [
+                  _buildAppBar(context, state.data),
+                ],
+                alignment: MainAxisAlignment.start,
+                axisSize: MainAxisSize.max,
+              );
+            }
+            return 0.heightBox;
+          },
+        ).p16().centered(),
+      ),
     );
   }
+
+  Widget _buildAppBar(BuildContext context, UserModel data) {
+    return VxBox(
+      child: HStack(
+        [
+          //USER Profile
+          HStack([
+            VxCircle(
+              radius: 56,
+              backgroundImage: DecorationImage(
+                image: NetworkImage(data.photoProfile!),
+                fit: BoxFit.cover,
+              ),
+            ),
+            16.widthBox,
+            "Selamat Datang,\n".richText.size(11).withTextSpanChildren([
+              data.username!.textSpan.size(14).bold.make(),
+            ]).make(),
+          ]).expand(),
+
+          //ICON Logout
+          IconButton(
+            onPressed: () {
+              BlocProvider.of<UserBloc>(context).add(LogOutUser());
+            },
+            icon: const Icon(
+              Icons.logout_rounded,
+              color: colorName.accentRed,
+            ),
+          )
+        ],
+        // alignment: MainAxisAlignment.spaceBetween,
+        // axisSize: MainAxisSize.max,
+      ),
+    ).make();
+  }
+
+  //TODO: Create Product Grid
+
 }
--- lib/src/screens/screens.dart
+++ lib/src/screens/screens.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:switchify/src/blocs/blocs.dart';
+import 'package:switchify/src/models/models.dart';
 import 'package:switchify/src/utilities/utilities.dart';
 import 'package:switchify/src/widgets/widgets.dart';
 import 'package:velocity_x/velocity_x.dart';
